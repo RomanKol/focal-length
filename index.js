e2ced15713acf79806ce649a305c290620774b1b
RomanKol <mail@kollatschny.net>
2018-04-08 12:03:22 +0200

    add initial app

---
+++ index.js
@@ -0,0 +1,15 @@
+/* eslint-disable
+  react/jsx-filename-extension,
+  import/no-named-as-default,
+  import/no-named-as-default-member
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app'),
+);
+
+module.hot.accept();
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+
+        <header>
+          <h1>Focal length comparison</h1>
+          <p>Compare and visualize different focal lengths on various sensors</p>
+        </header>
+
+      </main>
+    );
+  }
+}
+
+export default App;
