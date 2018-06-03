4813d245ca8bd9e90c990b307479841b3082f5d0
RomanKol <mail@kollatschny.net>
2018-06-03 17:34:30 +0200

    updated sensor layout; added sensorSize component

--- src/Cards/Sensor.jsx
+++ src/Cards/Sensor.jsx
@@ -1,55 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
+import styled from 'react-emotion';
 
 import store from '../store';
 
 import Card from './card';
 import { Table, Thead, Td } from '../components/table';
 import SensorSelect from '../components/sensorSelect';
+import SensorSize from '../components/sensorSize';
+
+const ColorCircle = styled.div`
+  width: 1em;
+  height: 1em;
+  background-color: ${props => props.color};
+  border-radius: 50%;
+  margin: 0 0 0 auto;
+`;
 
 const Sensor = observer(({ title }) => {
-  const { selectedSensors } = store;
+  const { selectedSensors, selectedSensorColors } = store;
+
+  const SelectedSensorTable = () => (
+    <React.Fragment>
+      <Table>
+        <Thead>
+          <tr>
+            <th />
+            {selectedSensors.map(sensor => <th key={sensor.name}>{sensor.name}</th>)}
+          </tr>
+        </Thead>
+
+        <tbody>
+          {Object.keys(selectedSensors[0]).filter(key => key !== 'name').map(key => (
+            <tr key={key}>
+              <Td><strong>{key}</strong></Td>
+              {selectedSensors.map(sensor => <Td key={sensor.name}>{sensor[key] || '-'}</Td>)}
+            </tr>
+          ))}
+          <tr>
+            <Td><strong>color</strong></Td>
+            {selectedSensorColors.map(color => <Td key={color}><ColorCircle color={color} /></Td>)}
+          </tr>
+        </tbody>
+
+      </Table>
+
+      <footer>
+        <small>
+          The units of the sensor diagonal, width and height are <i>mm</i>.
+          The unit of the sensor area is <i>mm²</i>.
+        </small>
+      </footer>
+
+    </React.Fragment>
+  );
 
   return (
     <Card>
-      <h3>{title}</h3>
+      <header>
+        <h3>{title}</h3>
+      </header>
 
       <section>
-        <section>
-          <SensorSelect />
-          {selectedSensors.length > 0
-            ? (
-              <React.Fragment>
-                <Table>
+        <SensorSelect />
 
-                  <Thead>
-                    <tr>
-                      <th />
-                      {selectedSensors.map(sensor => <th key={sensor.name}>{sensor.name}</th>)}
-                    </tr>
-                  </Thead>
-                  <tbody>
-                    {Object.keys(selectedSensors[0]).filter(key => key !== 'name').map(key => (
-                      <tr key={key}>
-                        <Td>{key}</Td>
-                        {selectedSensors.map(sensor => <Td key={sensor.name}>{sensor[key] || '-'}</Td>)}
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-                <footer>
-                  <small>
-                    The units of the sensor diagonal, width and height are <i>mm</i>.
-                    The unit of the sensor area is <i>mm²</i>.
-                  </small>
-                </footer>
-              </React.Fragment>
-            )
-            : <p>No sensor selected - Select sensors to compare them</p>
-          }
-        </section>
-        {/* <SensorSize /> */}
+        {selectedSensors.length > 0
+          ? <SelectedSensorTable />
+          : <p>No sensor selected - Select sensors to compare them</p>
+        }
+      </section>
+
+      <section>
+        <SensorSize />
+
+        <footer>
+          <small>The shown sensors dimensions are not to scale!</small>
+        </footer>
       </section>
     </Card>
   );
---
+++ src/components/sensorSize.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
+
+import Dimensions from 'react-dimensions';
+
+import store from '../store';
+
+const SensorSize = observer(({ aspect, containerWidth: width }) => {
+  const height = width * (aspect ** -1);
+  const { selectedSensors, selectedSensorColors } = store;
+
+  // calculate scaling, if sensor dimension is bigger than the the rect, scale down, else scale up
+  const maxWidth = Math.max(...selectedSensors.map(sensor => sensor.width));
+  const maxHeight = Math.max(...selectedSensors.map(sensor => sensor.height));
+
+  // shift/translate the sensors to center them
+  const scaling = Math.min(width / maxWidth, height / maxHeight);
+  const xShift = Math.round((width - (maxWidth * scaling)) / 2);
+  const yShift = Math.round((height - (maxHeight * scaling)) / 2);
+
+  const Rect = (sensor, i) => {
+    const strokeWidth = 1;
+    const styles = {
+      fill: selectedSensorColors[i],
+      strokeWidth,
+      stroke: '#000',
+    };
+
+    const x = Math.round(sensor.width * scaling);
+    const y = Math.round(sensor.height * scaling);
+
+    return (
+      <rect
+        key={sensor.name}
+        x={0 + strokeWidth + xShift}
+        y={Math.round(height - y) + strokeWidth + yShift}
+        width={x - (strokeWidth * 2)}
+        height={y - (strokeWidth * 2)}
+        fill="none"
+        {...styles}
+      />
+    );
+  };
+
+  return (
+    <section>
+      <svg viewBox={`0,0,${width},${height}`}>
+        {selectedSensors.map(Rect).reverse()}
+      </svg>
+    </section>
+  );
+});
+
+SensorSize.propTypes = {
+  aspect: PropTypes.number,
+  containerWidth: PropTypes.number.isRequired,
+};
+
+SensorSize.defaultProps = {
+  aspect: 3 / 2,
+};
+
+export default Dimensions()(SensorSize);
